effedacb4bcbddc52b69bf5aa8b0f2f2a24687f8
Malachi Gray <graymd.cfi@gmail.com>
2016-07-12 22:47:44 -0400

    setting up snake game - grid rendering + starting to build snake

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+$('document').ready(function(){
+  gameboard.render();
+
+});
+
+gameboard = {
+  singleGrid: '<div class="gamesquare"></div>',
+  setFullGrid: function(){
+    for (var i=0; i<40; i++){
+      for (var j=0; j<40; j++){
+      $('#gameboard').append(this.singleGrid);
+      }
+    }
+    $('#gameboard').append(snake.render());
+  },
+  render: function(){
+    return this.setFullGrid();
+  }
+
+}
+
+snake = {
+  render: function(){
+    return this.snakeHead + this.snakeBody;
+  },
+  snakeHead: '<div class="snakeHead">0</div>',
+  snakeBody: '<div class="snakeBody">X</div>'
+}
